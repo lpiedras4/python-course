547c9af585306fdce0f15f44d5fcc774de58530c
lpiedras4 <piedras.leo@gmail.com>
2025-12-10 20:50:12 -0800

    Completed Introduction to Python lesson exercises (variables, strings, variable types, python shell

---
+++ Introduction/day1.py
@@ -0,0 +1,27 @@
+# basic operations
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+#Strings
+name = "Leonardo"
+family_name = "Piedras"
+country = "Mexico"
+print(name)
+print(family_name)
+print(country)
+print("I am enjoying 30 days of python")
+
+#Data type check
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh','Python','Finland']))
+print(type("Leonardo"))
+print(type("Piedras"))
+print(type("Mexico"))
--- hello-world/app.py
+++ hello-world/app.py
@@ -1,3 +1,3 @@
 print("Hello world")
 print("*" * 10)
-
+x=1
