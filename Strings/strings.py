81e5c5e19b639938eea5412db946574a83ad5473
lpiedras4 <piedras.leo@gmail.com>
2025-12-11 20:52:04 -0800

    Finished string methods exercises

--- Strings/strings.py
+++ Strings/strings.py
@@ -1,2 +1,19 @@
 string = ["Coding", "For", "All"]
+result = " ".join(string)
+company = "Apple"
 print(' '.join(string))
+print(len(company))
+print(company.upper())
+print(company.lower())
+print(result.capitalize())
+print(result.title())
+print(result.swapcase())
+print(string[slice(0)])
+print(result.find("or"))
+print(result.find("a"))
+new_string = result.replace("Coding","Python")
+print(new_string)
+print(new_string.replace("For All","For Everyone"))
+print(result.split(","))
+companies = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+print(companies.split(","))
